54d27dad19db18e111097740c7444fcb511987b2
argades <osoriodeleon@gmail.com>
2024-06-04 12:31:56 +0200

    [ADD] obteniendo las imagenes desde la API

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,5 +1,24 @@
 
+
+const getGif = async () => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&api_key=Oe2iSNIZXGbeqNbR9wHhnDe7TDVZfCR0&limit=20`;
+  const response = await fetch( url );
+
+  const { data } = await response.json();
+
+  const gifs = data.images( img => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url
+  }));
+
+  console.log(gifs);
+  return gifs;
+}
+
 export const GifGrid = ( { category } ) => {
+
+  getGif(category);
 
   return (
     <>
