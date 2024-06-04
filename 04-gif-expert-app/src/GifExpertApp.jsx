e2a0a0e206621232b6bc4f17f4610762d0e8f859
argades <osoriodeleon@gmail.com>
2024-06-04 12:06:11 +0200

    [ADD] componente GifGrid

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
@@ -17,22 +18,17 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      {/* Titulo */}
       <h1>GifExpertApp</h1>
 
-      {/* Input */}
       <AddCategory 
         onNewCategory={ (value) => onAddCategory(value) }
       />
 
-      {/* Listado */}
-      <ol>
-        { 
-          categories.map( category => {
-            return <li key={ category }>{ category }</li>
-          }) 
-        }
-      </ol>
+      { 
+        categories.map( category => (
+          <GifGrid key={category} category={ category }/>
+        )) 
+      }
     </>
   )
 }
---
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -0,0 +1,10 @@
+
+export const GifGrid = ( { category } ) => {
+
+  return (
+    <>
+      <h1>{ category }</h1>
+      <p>KK</p>
+    </>
+  )
+}
