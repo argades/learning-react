87a9fbd7cfcdf07fd1b355f66fe422bb3dbe4047
argades <osoriodeleon@gmail.com>
2024-06-06 03:28:46 +0200

    [ADD] Test evaluar comportamiento basado en resultado

---
+++ 04-gif-expert-app/tests/hooks/useFetchGif.test.js
@@ -0,0 +1,34 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+describe('Testing custom hooks useFetchGif', () => {
+  
+  test('should return initial state', () => {
+    
+    const { result } = renderHook(() => useFetchGifs('Kaliman'));
+
+    const { images, isLoading } = result.current;
+    
+    // Validar el estado inicial
+    expect( images.length ).toBe(0);
+    expect( isLoading ).toBeTruthy();
+    
+  });
+  
+  test('should return images array and isLoading false', async() => {
+    
+    const { result } = renderHook(() => useFetchGifs('Kaliman'));
+
+    await waitFor(
+      () => expect( result.current.images.length ).toBeGreaterThan(0)
+    );
+
+    const { images, isLoading } = result.current;
+    
+    // Validar el estado inicial
+    expect( images.length ).toBeGreaterThan(0);
+    expect( isLoading ).toBeFalsy();
+
+  });
+  
+})
